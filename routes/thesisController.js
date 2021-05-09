5c7490bceb9dc26b256d1918705ed5f4af1f17bd
Raka Gunarto <rakagunarto@gmail.com>
2021-05-09 20:06:06 +0800

    Pre-process authors fields correctly

--- routes/thesisController.js
+++ routes/thesisController.js
@@ -19,22 +19,25 @@ const thesisStorage = new GridFsStorage({
                 const filename = buf.toString('hex') + '_' + file.originalname;
                 const fileInfo = {
                     filename: filename,
-                    bucketName: 'thesesFiles'
+                    bucketName: 'thesesFiles',
                 };
                 resolve(fileInfo);
             });
         });
-    }
+    },
 });
 
 const thesisUpload = multer({
     storage: thesisStorage,
-    fileFilter: function(req, file, cb) {
-        if (path.extname(file.originalname).toLowerCase() !== '.pdf' || file.mimetype !== 'application/pdf') {
+    fileFilter: function (req, file, cb) {
+        if (
+            path.extname(file.originalname).toLowerCase() !== '.pdf' ||
+            file.mimetype !== 'application/pdf'
+        ) {
             return cb(new multer.MulterError(400, 'File type is not pdf!'));
         }
         return cb(null, true);
-    }
+    },
 }).single('file');
 
 // TODO: add Thesis array field in the Profile model. Needed or not?
@@ -48,7 +51,7 @@ const thesisUpload = multer({
  * @param req.body.file is an optional pdf file
  * @return res.body.id is the thesis submission id.
  */
-exports.new =  function (req, res) {
+exports.new = function (req, res) {
     thesisUpload(req, res, async function (err) {
         // FIXME: Decide on behaviour (who is allowed to submit whose)
         // if (!req.body.authors.includes(String(res.locals.oauth.token.user._id)) &&
@@ -60,16 +63,19 @@ exports.new =  function (req, res) {
 
         if (err) {
             return res.status(err.code).json({
-                message: err.field
-            })
-        }
-        if (!req.body.authors.includes(req.body.correspondingAuthor)) {
-            return res.status(400).json({
-                message: 'Corresponding Author is not included in the list of authors!'
+                message: err.field,
             });
         }
 
-        let thesis = new Thesis(req.body);
+        const authors = JSON.parse(req.body.authors);
+        if (!authors.includes(req.body.correspondingAuthor)) {
+            return res.status(400).json({
+                message:
+                    'Corresponding Author is not included in the list of authors!',
+            });
+        }
+
+        let thesis = new Thesis({ ...req.body, authors: authors });
         thesis = await processThesis(req, thesis);
         thesis.save(function (err) {
             if (err) {
@@ -77,15 +83,13 @@ exports.new =  function (req, res) {
                     message: err.message,
                 });
             }
-            return res
-                .status(200)
-                .json({
-                    message: 'New thesis submitted!',
-                    id: thesis._id,
-                });
+            return res.status(200).json({
+                message: 'New thesis submitted!',
+                id: thesis._id,
+            });
         });
-    })
-}
+    });
+};
 
 async function processThesis(req, thesis) {
     if (req.file) {
@@ -94,23 +98,29 @@ async function processThesis(req, thesis) {
     }
 
     try {
-        await Profile.findById(thesis.correspondingAuthor, { _id: 1}, (err, profile) => {
-            if (profile === null || (err && err.name === 'CastError')) {
-                thesis.correspondingAuthor = req.body.correspondingAuthor;
-            } else {
-                thesis.correspondingAuthor = mongoose.Types.ObjectId(profile._id);
+        await Profile.findById(
+            thesis.correspondingAuthor,
+            { _id: 1 },
+            (err, profile) => {
+                if (profile === null || (err && err.name === 'CastError')) {
+                    thesis.correspondingAuthor = req.body.correspondingAuthor;
+                } else {
+                    thesis.correspondingAuthor = mongoose.Types.ObjectId(
+                        profile._id
+                    );
+                }
             }
-        });
+        );
     } catch {}
 
-    let authors = []
+    let authors = [];
     for (const author of thesis.authors) {
         try {
-            await Profile.findById(author, { _id: 1}, (err, profile) => {
+            await Profile.findById(author, { _id: 1 }, (err, profile) => {
                 if ((err && err.name === 'CastError') || profile === null) {
                     authors.push(author);
                 } else {
-                    authors.push(profile._id)
+                    authors.push(profile._id);
                 }
             });
         } catch {}
@@ -130,47 +140,49 @@ async function processThesis(req, thesis) {
  * @param req.body.file is an optional pdf file
  * @return res.body.id is the thesis submission id.
  */
-exports.update =  function (req, res) {
+exports.update = function (req, res) {
     thesisUpload(req, res, async function (err) {
-        if (!req.body.authors.includes(String(res.locals.oauth.token.user._id)) &&
-            !res.locals.oauth.token.user.roles.includes('thesisAdmin')) {
+        if (
+            !req.body.authors.includes(
+                String(res.locals.oauth.token.user._id)
+            ) &&
+            !res.locals.oauth.token.user.roles.includes('thesisAdmin')
+        ) {
             return res.status(403).json({
-                message: 'You are trying to update thesis that you did not contribute in!'
-            })
+                message:
+                    'You are trying to update thesis that you did not contribute in!',
+            });
         }
 
         if (err) {
             return res.status(err.code).json({
-                message: err.field
-            })
+                message: err.field,
+            });
         }
         if (!req.body.authors.includes(req.body.correspondingAuthor)) {
             return res.status(400).json({
-                message: 'Corresponding Author is not included in the list of authors!'
+                message:
+                    'Corresponding Author is not included in the list of authors!',
             });
         }
 
-        Thesis.findById(req.params.id,  async function (err, thesis) {
+        Thesis.findById(req.params.id, async function (err, thesis) {
             thesis.set(req.body);
-            thesis = await processThesis(req, thesis)
+            thesis = await processThesis(req, thesis);
             thesis.save(function (err) {
                 if (err) {
                     return res.status(400).json({
                         message: err.message,
                     });
                 }
-                return res
-                    .status(200)
-                    .json({
-                        message: 'Thesis updated!',
-                        id: thesis._id,
-                    });
-            })
-
-        })
-
-    })
-}
+                return res.status(200).json({
+                    message: 'Thesis updated!',
+                    id: thesis._id,
+                });
+            });
+        });
+    });
+};
 
 /**
  * Retrieves information of a thesis, given the thesis submission ID.
@@ -186,7 +198,7 @@ exports.update =  function (req, res) {
  * @return res.body.data.originalFileName if there is a pdf file uploaded with this submission
  */
 exports.view = function (req, res) {
-    Thesis.findById(req.params.id, {__v: 0 }, async (err, thesis) => {
+    Thesis.findById(req.params.id, { __v: 0 }, async (err, thesis) => {
         if (thesis === null || (err && err.name === 'CastError')) {
             return res.status(404).json({
                 message: 'Thesis ID not found!',
@@ -197,27 +209,41 @@ exports.view = function (req, res) {
             });
         } else {
             try {
-                await Profile.findById(thesis.correspondingAuthor, { _id: 1, fullName: 1}, (err, profile) => {
-                    if (profile === null || (err && err.name === 'CastError')) {
-                        thesis.correspondingAuthor = {
-                            fullName: thesis.correspondingAuthor
+                await Profile.findById(
+                    thesis.correspondingAuthor,
+                    { _id: 1, fullName: 1 },
+                    (err, profile) => {
+                        if (
+                            profile === null ||
+                            (err && err.name === 'CastError')
+                        ) {
+                            thesis.correspondingAuthor = {
+                                fullName: thesis.correspondingAuthor,
+                            };
+                        } else {
+                            thesis.correspondingAuthor = profile;
                         }
-                    } else {
-                        thesis.correspondingAuthor = profile;
                     }
-                });
+                );
             } catch {}
 
-            let authors = []
+            let authors = [];
             for (const author of thesis.authors) {
                 try {
-                    await Profile.findById(author, { _id: 1, fullName: 1}, (err, profile) => {
-                        if (profile === null || (err && err.name === 'CastError')) {
-                            authors.push({ fullName: author });
-                        } else {
-                            authors.push(profile);
+                    await Profile.findById(
+                        author,
+                        { _id: 1, fullName: 1 },
+                        (err, profile) => {
+                            if (
+                                profile === null ||
+                                (err && err.name === 'CastError')
+                            ) {
+                                authors.push({ fullName: author });
+                            } else {
+                                authors.push(profile);
+                            }
                         }
-                    });
+                    );
                 } catch {}
             }
             thesis.authors = authors;
@@ -225,10 +251,10 @@ exports.view = function (req, res) {
             return res.status(200).json({
                 message: 'Thesis details returned successfully!',
                 data: thesis,
-            })
+            });
         }
-    })
-}
+    });
+};
 
 /**
  * Retrieves the uploaded pdf file associated with the thesis submission.
@@ -238,39 +264,45 @@ exports.view = function (req, res) {
  */
 exports.viewFile = function (req, res) {
     const bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
-        bucketName: 'thesesFiles'
-    })
-    Thesis.findById(req.params.id, {fileId: 1, originalFileName: 1}, (err, thesis) => {
-        if (thesis === null || (err && err.name === 'CastError')) {
-            return res.status(404).json({
-                message: 'Thesis ID not found',
-            });
-        } else if (thesis.fileId === undefined) {
-            return res.status(404).json({
-                message: 'There is no file associated with this Thesis ID'
-            });
-        } else if (err) {
-            return res.status(500).json({
-                message: err.message,
-            });
-        }
-        const stream =  bucket.openDownloadStream(new mongoose.Types.ObjectId(thesis.fileId));
-        stream.on('error', err => {
-            if (err.code === 'ENOENT') {
+        bucketName: 'thesesFiles',
+    });
+    Thesis.findById(
+        req.params.id,
+        { fileId: 1, originalFileName: 1 },
+        (err, thesis) => {
+            if (thesis === null || (err && err.name === 'CastError')) {
                 return res.status(404).json({
-                    message: 'File not found'
+                    message: 'Thesis ID not found',
+                });
+            } else if (thesis.fileId === undefined) {
+                return res.status(404).json({
+                    message: 'There is no file associated with this Thesis ID',
+                });
+            } else if (err) {
+                return res.status(500).json({
+                    message: err.message,
                 });
             }
-            return res.status(500).json({
-                message: err.message
+            const stream = bucket.openDownloadStream(
+                new mongoose.Types.ObjectId(thesis.fileId)
+            );
+            stream.on('error', (err) => {
+                if (err.code === 'ENOENT') {
+                    return res.status(404).json({
+                        message: 'File not found',
+                    });
+                }
+                return res.status(500).json({
+                    message: err.message,
+                });
             });
-        });
-        res.writeHead(200, {
-            'Content-Disposition': 'filename=' + thesis.originalFileName,
-        });
-        stream.pipe(res);
-    })
-}
+            res.writeHead(200, {
+                'Content-Disposition': 'filename=' + thesis.originalFileName,
+            });
+            stream.pipe(res);
+        }
+    );
+};
 
 /**
  * Deletes thesis submission and the associated pdf file (if uploaded).
@@ -290,18 +322,21 @@ exports.delete = function (req, res) {
             });
         }
         if (thesis.fileId) {
-            const bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
-                bucketName: 'thesesFiles'
-            })
-            bucket.delete(new mongoose.Types.ObjectId(thesis.fileId), err => {
+            const bucket = new mongoose.mongo.GridFSBucket(
+                mongoose.connection.db,
+                {
+                    bucketName: 'thesesFiles',
+                }
+            );
+            bucket.delete(new mongoose.Types.ObjectId(thesis.fileId), (err) => {
                 if (err) {
                     if (err.message.startsWith('FileNotFound')) {
                         return res.status(404).json({
-                            message: 'File not found'
+                            message: 'File not found',
                         });
                     }
                     return res.status(500).json({
-                        message: err.message
+                        message: err.message,
                     });
                 }
             });
@@ -310,15 +345,16 @@ exports.delete = function (req, res) {
         Thesis.remove({ _id: thesis._id }, (err) => {
             if (err) {
                 return res.status(500).json({
-                    message: err.message
+                    message: err.message,
                 });
             }
             return res.status(200).json({
-                message: 'Thesis submission (and file, if uploaded) is deleted.'
-            })
+                message:
+                    'Thesis submission (and file, if uploaded) is deleted.',
+            });
         });
-    })
-}
+    });
+};
 
 /**
  * Find permission for the requested action and role
